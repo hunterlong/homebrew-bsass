4026cc64a760ac80d79a933108726fe1414efdb1
hunterlong <info@socialeck.com>
2018-07-08 00:09:38 +0000

    New Homebrew v0.16 726948a

--- bsass.rb
+++ bsass.rb
@@ -1,23 +1,23 @@
 class Bsass < Formula
   homepage 'https://github.com/hunterlong/bsass'
   desc "bsass is much like the normal sass"
-  homepage "https://github.com/hunterlong/bsass"
-  version "0.15"
+  homepage "https://bsass.app"
+  version "0.16"
   if OS.mac?
     if Hardware::CPU.is_64_bit?
-      url "https://github.com/hunterlong/bsass/releases/download/v0.15/bsass-osx-x64.tar.gz"
-      sha256 "704c9349000911f5f69fd3dc2578f1a44c43b1b163761091d5283b935fdf310f"
+      url "https://github.com/hunterlong/bsass/releases/download/v0.16/bsass-osx-x64.tar.gz"
+      sha256 "adeb00b51ef1f69b4210b62e4aab1cebb90090260e424b53469a287b44d6484e"
     else
-      url "https://github.com/hunterlong/bsass/releases/download/v0.15/bsass-osx-x32.tar.gz"
-      sha256 "7d81d5b4f69dac8b8835d404b281a34b4708aacec1c0291f16ac1ac0d321c79d"
+      url "https://github.com/hunterlong/bsass/releases/download/v0.16/bsass-osx-x32.tar.gz"
+      sha256 "bce5bde42e5cd988a6747cfd336f23b50744c20218886293b560d8ea2d83ca58"
     end
   elsif OS.linux?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/hunterlong/bsass/releases/download/v0.15/bsass-linux-x64.tar.gz"
-        sha256 "64c5e2effe71d322ab49f83eb6bf7e1b51c2cbc7df65d07b1ec90b82e432735a"
+        url "https://github.com/hunterlong/bsass/releases/download/v0.16/bsass-linux-x64.tar.gz"
+        sha256 "a26eba4c6f498dbb76a388bdfacecbb4dd71f182c9261cd208c4878f4c24241b"
       else
-        url "https://github.com/hunterlong/bsass/releases/download/v0.15/bsass-linux-x32.tar.gz"
-        sha256 "eb5950fb3ade6fff3d28a245850eb0482fd6377ab0d669d0fa5e29010190255f"
+        url "https://github.com/hunterlong/bsass/releases/download/v0.16/bsass-linux-x32.tar.gz"
+        sha256 "db0edaa7e1579ecbf6bc236a2658e731582310948c5f68ae824819522c0fd133"
       end
   end
 
